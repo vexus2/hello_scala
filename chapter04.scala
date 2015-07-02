723747dc7a532d128c7ee5b3d18ee95492a18a2f
vexus2 <rinrin900@gmail.com>
2015-07-02 16:13:57 +0900

    finish 4.3

--- chapter04.scala
+++ chapter04.scala
@@ -8,3 +8,20 @@ class ChecksumAccumulator {
 val acc = new ChecksumAccumulator
 val csa = new ChecksumAccumulator
 
+import scala.collection.mutable.Map
+object ChecksumAccumulator {
+  private val cache = Map[String, Int]()
+  def calculate(s:String): Int =
+  if (cache.contains(s))
+    cache(s)
+  else{
+    val acc = new ChecksumAccumulator
+    for(c <- s)
+      acc.add(c.toByte)
+    val cs = acc.checksum()
+    cache += (s -> cs)
+    cs
+  }
+}
+
+ChecksumAccumulator.calculate("Every value is an object.")
