2635fad85f0cb5b80d23b11f1fce97a9a3a74ce7
vexus2 <rinrin900@gmail.com>
2015-07-02 12:23:44 +0900

    step2

--- chapter03.scala
+++ chapter03.scala
@@ -17,3 +17,18 @@ greetStrings.update(0, "Hello")
 
 val numNames = Array("zero", "one", "two")
 val numNames2 = Array.apply("zero", "one", "two")
+
+val oneTwoThree = List(1, 2, 3)
+val oneTwo = List(1, 2)
+val threeFour = List(3, 4)
+val oneTwoThreeFour = oneTwo ::: threeFour
+println(oneTwo + " and " + threeFour + " were not mutated.")
+println("Thus, " + oneTwoThreeFour + " is a new list.")
+
+val twoThree2 = List(2, 3)
+val oneTwoThree2 = 1 :: twoThree2
+println(oneTwoThree2)
+
+val oneTwoThree3 = 1 :: 2 :: 3 :: Nil
+println(oneTwoThree3)
+oneTwoThree3.foreach(s => println(s))
