aff66243f03c1ff974826b1d93f7fbaea0b74f02
vexus2 <rinrin900@gmail.com>
2015-07-02 16:04:35 +0900

    finish 4.2

---
+++ chapter04.scala
@@ -0,0 +1,10 @@
+class ChecksumAccumulator {
+  private var sum = 0
+
+  def add(b: Byte) { sum += b }
+  def checksum(): Int = ~(sum & 0xFF) + 1
+}
+
+val acc = new ChecksumAccumulator
+val csa = new ChecksumAccumulator
+
