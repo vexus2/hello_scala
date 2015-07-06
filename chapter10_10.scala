238b3745cfd6972880ca0cce3022e1cf9e205a38
vexus2 <rinrin900@gmail.com>
2015-07-06 18:21:51 +0900

    finish 10.10

---
+++ chapter10_10.scala
@@ -0,0 +1,13 @@
+abstract class Element {
+  def contents: Array[String]
+
+  def height: Int = contents.length
+
+  def width: Int = if (height == 0) 0 else contents(0).length
+}
+
+final class ArrayElement extends Element {
+  override def demo(): Unit = {
+    println("ArrayElement's implementation invoked")
+  }
+}
