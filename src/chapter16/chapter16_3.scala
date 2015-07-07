d35e68f2aa53d801bf6193b997a2eb9b3585bf05
vexus2 <rinrin900@gmail.com>
2015-07-07 15:25:39 +0900

    finish 16.3

---
+++ src/chapter16/chapter16_3.scala
@@ -0,0 +1,4 @@
+val fruit = "apples" :: ("oranges" :: ("pears" :: Nil))
+val nums = 1 :: (2 :: (3 :: (4 :: Nil)))
+val diag3 = (1:: (0 :: (0 :: Nil))) :: (0 :: (1 :: (0 :: Nil))) :: (0 :: (0 :: (1 :: Nil))) :: Nil
+val empty = Nil
