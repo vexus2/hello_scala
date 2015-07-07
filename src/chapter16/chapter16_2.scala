e276d9e337b778ec73af4f93339b46ec8d3aed9a
vexus2 <rinrin900@gmail.com>
2015-07-07 15:20:48 +0900

    finish 16.2

---
+++ src/chapter16/chapter16_2.scala
@@ -0,0 +1,8 @@
+val fruit = List[String] = List("apples", "oranges", "pears")
+
+val nums: List[Int] = List(1, 2, 3, 4)
+val diag3: List[List[Int]] = List(List(1,0,0), List(0,1,0), List(0,0,1))
+val empty: List[Nothing] = List()
+
+// List()はList[String]でもある
+val xs: List[String] = List()
