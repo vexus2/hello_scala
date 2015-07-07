1e1934c830eef7622b13b130eb7683919a479968
vexus2 <rinrin900@gmail.com>
2015-07-07 15:32:35 +0900

    finish 16.4

---
+++ src/chapter16/chapter16_4.scala
@@ -0,0 +1,27 @@
+val fruit = "apples" :: ("oranges" :: ("pears" :: Nil))
+val nums = 1 :: (2 :: (3 :: (4 :: Nil)))
+val diag3 = (1 :: (0 :: (0 :: Nil))) :: (0 :: (1 :: (0 :: Nil))) :: (0 :: (0 :: (1 :: Nil))) :: Nil
+val empty = Nil
+
+empty.isEmpty
+fruit.isEmpty
+fruit.head
+fruit.tail
+diag3.tail
+
+fruit :: fruit
+fruit ++ fruit
+
+nums :: nums
+nums ++ nums
+
+def isort(xs: List[Int]): List[Int] =
+  if (xs.isEmpty) Nil
+  else insert(xs.head, isort(xs.tail))
+
+def insert(x: Int, xs: List[Int]): List[Int] =
+  if (xs.isEmpty || x <= xs.head) x :: xs
+  else xs.head :: insert(x, xs.tail)
+
+
+
