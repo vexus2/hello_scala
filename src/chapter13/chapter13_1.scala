e09096332b4c36b575dffe76050d33ce9050ef72
vexus2 <rinrin900@gmail.com>
2015-07-07 14:07:06 +0900

    finish 13.1

---
+++ src/chapter13/chapter13_1.scala
@@ -0,0 +1,15 @@
+package bobsrockets.navigation
+class Navigator
+
+package other.bobsrockets.navigation {
+  class Navigator
+}
+
+package bobsrockets {
+  package navigation {
+    class Navigator
+    package tests {
+      class NavigatorSuite
+    }
+  }
+}
